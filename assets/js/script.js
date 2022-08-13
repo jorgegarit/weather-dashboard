e25a68d2951689d38b2581427be0ef4e43e9a390
Jorge Garit <jorgegarit@gmail.com>
2022-08-13 17:40:37 -0400

    added the name of the city when button is pressed

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,26 @@ const apiKey = "746e01aa22c61fa5a613107f004407af";
 
 var inputFieldEl = document.getElementById("cityInput");
 var todaysWeatherEl = document.getElementById("todaysWeather");
-
-// variables for the search history 
-var inputCities = [];
 var inputCitiesEl = document.getElementById("searchList");
+
+
+var inputCities = [];
+
 
 
 
 function generateForecast(query) {
-    $.ajax
+    // clear the content in the forecats display for todays weather
+    todaysWeatherEl.innerHTML = "";
+
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&APPID=" + apiKey,
+        success: function(res) {
+            todaysWeatherEl.innerHTML = "<h3 class='cityName'>" + res.name + "</h3>"
+            // todaysWeatherEl.innerHTML =  + res.name + '</h3>';
+
+        }
+        
+    });
+    inputFieldEl.value = '';
 }
