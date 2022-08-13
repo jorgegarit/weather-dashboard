942ae5d7ac47c63cc83e420cff5f8e3c467c54f2
Jorge Garit <jorgegarit@gmail.com>
2022-08-13 17:17:07 -0400

    added the basis for my variable calls before using ajax to generate forecast

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// open weather api key
+const apiKey = "746e01aa22c61fa5a613107f004407af";
+
+var inputFieldEl = document.getElementById("cityInput");
+var todaysWeatherEl = document.getElementById("todaysWeather");
+
+// variables for the search history 
+var inputCities = [];
+var inputCitiesEl = document.getElementById("searchList");
+
+
+
+function generateForecast(query) {
+    $.ajax
+}
